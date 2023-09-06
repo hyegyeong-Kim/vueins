4ff23f69ad4828edc6d486c9ebea5c7a2d782433
hgkim <hgkim@rianag.com>
2023-09-06 17:01:48 +0900

    axios 까지 연습

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :데이터들="데이터들"/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  name: 'Container',
+  components: {
+    Post,
+  },
+  props : {
+    데이터들 : Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
